4248e4f2977de20bd4e900f0c97a5e2bfb7c3201
Alumno <borjajimgar02@gmail.com>
2025-02-12 16:26:51 +0100

    feat: creacion de las clases

---
+++ app/src/main/java/com/example/exaddfebrero/feature/seta/domain/Seta.kt
@@ -0,0 +1,11 @@
+package com.example.exaddfebrero.feature.seta.domain
+
+class Seta (
+    val  idSeta : String,
+    val name : String,
+    val foto : String,
+    val latitud : String,
+    val longitud : String,
+    val fecha : String,
+    val hora : String
+)
